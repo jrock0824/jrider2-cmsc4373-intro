976a43bd425b26d12e4ba57b023729f69746f5c5
Jaylen Rider <jaylenr2002@icloud.com>
2025-08-20 18:19:06 -0500

    add row

---
+++ public/app.js
@@ -0,0 +1,6 @@
+//console.log('Hello from app.js!');
+//console.log('This is a simple JavaScript file included in the HTML.');
+
+const b1 = document.querySelector('button')
+b1.onclick = function buttonClick(e) {
+    console.log('Button clicked!');}
