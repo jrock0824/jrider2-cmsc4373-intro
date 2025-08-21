1a162ff4946dcd819d870ff7c2b7e49d9b26eb95
Jaylen Rider <jaylenr2002@icloud.com>
2025-08-20 20:48:55 -0500

    final

--- public/app.js
+++ public/app.js
@@ -3,4 +3,17 @@
 
 const b1 = document.querySelector('button')
 b1.onclick = function buttonClick(e) {
-    console.log('Button clicked!');}
+    console.log('Button clicked!');
+    const rndNum = Math.floor(Math.random() * 10) + 1;
+    const squareNum = rndNum * rndNum;
+
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+        <td>${rndNum}</td>
+
+        <td>${squareNum}</td>`;
+
+        const tbody = document.querySelector('tbody');
+        tbody.appendChild(newRow);
+
+}
